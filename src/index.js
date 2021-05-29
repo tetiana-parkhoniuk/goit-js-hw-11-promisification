bf91f7545d24964bdf5b4f659c537b262a1e2522
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-05-29 09:51:42 +0300

    Adds resolve fixes.

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ const makeTransaction = (transaction) => {
             const canProcess = Math.random() > 0.3;
 
             if (canProcess) {
-                resolve(transaction.id, delay);
+                resolve([transaction.id, delay]);
             }
 
             reject(transaction.id);
@@ -63,7 +63,7 @@ const makeTransaction = (transaction) => {
     })
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
